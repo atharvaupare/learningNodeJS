a52003025ff93aec5198da7e633f6ff3228d7e05
atharvaupare <atharvaupare5@gmail.com>
2024-01-21 01:29:29 +0530

    refactor: aggregation pipeline and tourSchema

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,6 +10,7 @@ const {
   checkBody,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require('./../controllers/tourController');
 
 // router.param('id', checkID);
@@ -19,6 +20,7 @@ const {
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
